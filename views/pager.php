1dd7f4fe0b0e717fbac2adaedde4bf67b748adf9
Jason Orrill <jason_orrill@brown.edu>
2011-06-16 14:50:46 +0000

    - Pulled pager code out of tabledata into its own file so that we can more easily include it elsewhere. It is still included in tabledata, just via the new external file.
    git-svn-id: https://svn.brown.edu/svn/doc/k3_doc_module/trunk@686 08eec777-141c-45f6-c71f-d277ddbd00ce

---
+++ views/pager.php
@@ -0,0 +1,15 @@
+<div id="pager" class="pager">
+	<form>
+		<a class="first">|&lt;</a>
+		<a class="prev">&lt;</a>
+		<input type="text" class="pagedisplay" />
+		<a class="next">&gt;</a>
+		<a class="last">&gt;|</a>
+		<select class="pagesize">
+			<option value="25">25</option>
+			<option value="50" selected="selected">50</option>
+			<option value="100">100</option>
+			<option value="250">250</option>
+		</select>
+	</form>
+</div>
--- views/tabledata.php
+++ views/tabledata.php
@@ -26,45 +26,32 @@ if( isset( $filter_fields ) && isset( $filter_model)) {
 
 if( count( $table_data ) > 0 ) {
 	print("<div id='datatable'>");
-		$table = new Util_Table($table_data, 'id="sortableTable", class="tablesorter"') ;
+	$table = new Util_Table($table_data, 'id="sortableTable", class="tablesorter"') ;
 
-		if( isset( $action_specs )) {
-			$table->add_actions_column($action_specs) ;
-		}
+	if( isset( $action_specs )) {
+		$table->add_actions_column($action_specs) ;
+	}
 
-		if( isset( $column_titles )) {
-			$table->set_column_titles( $column_titles ) ;
-		}
+	if( isset( $column_titles )) {
+		$table->set_column_titles( $column_titles ) ;
+	}
 
-		if( isset( $column_filter )) {
-			$table->set_column_filter( $column_filter ) ;
-		}
+	if( isset( $column_filter )) {
+		$table->set_column_filter( $column_filter ) ;
+	}
 
-		if( isset( $column_header_classes )) {
-			$table->set_header_classes( $column_header_classes ) ;
-		}
-		
-		if( isset( $format_specs )) {
-			$table->set_formats($format_specs) ;
-		}
-		$table->render(TRUE) ;
-?>
-		<div id="pager" class="pager">
-			<form>
-				<a class="first">|&lt;</a>
-				<a class="prev">&lt;</a>
-				<input type="text" class="pagedisplay" />
-				<a class="next">&gt;</a>
-				<a class="last">&gt;|</a>
-				<select class="pagesize">
-					<option value="25">25</option>
-					<option value="50" selected="selected">50</option>
-					<option value="100">100</option>
-					<option value="250">250</option>
-				</select>
-			</form>
-		</div>
-<?php
+	if( isset( $column_header_classes )) {
+		$table->set_header_classes( $column_header_classes ) ;
+	}
+
+	if( isset( $format_specs )) {
+		$table->set_formats($format_specs) ;
+	}
+	$table->render(TRUE) ;
+
+	$pager = View::factory('pager') ;
+	echo $pager->render() ;
+
 	print("</div>") ;
 } else {
 	print("<div class='nodata'>No results found.</div>") ;
