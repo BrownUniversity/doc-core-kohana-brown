b7967226fa5b1d0a8d31baf51520be23b8dbb1db
Jason Orrill <jason_orrill@brown.edu>
2012-06-08 19:46:57 +0000

    - Pulled more robust JSON-generating code out of ASK into the DOC module. So far I think most apps have their own JSON code, but we should be able to replace those with this.
    git-svn-id: https://svn.brown.edu/svn/doc/k3_doc_module/trunk@3163 08eec777-141c-45f6-c71f-d277ddbd00ce

--- views/rest/json/output.php
+++ views/rest/json/output.php
@@ -1,25 +1,42 @@
 <?php
-// TODO: Modify this to be cleaner (fewer in_array() calls for one) and more flexible-- some recursion is probably called for here...
-
 	$_output = '' ;
 
-	$as_json_supported = array('ORM', 'Database_Result') ;
+	// $as_json_supported = array('ORM', 'Database_Result') ;
 
 	if(is_object( $data ) && (in_array( 'ORM', class_parents( $data )) || in_array('Database_Result', class_parents( $data )))) {
 		$_output = $data->as_json() ;
 	} elseif( is_array( $data )) {
-		foreach( $data as $key => $value ) {		
-			if( in_array( 'ORM', class_parents( $value ))) {
-				$data[ $key ] = $value->as_array() ;
-			} elseif( in_array( 'Database_Result', class_parents( $value ))) {
-				$data[ $key ] = $value->as_complete_array() ;
-			}
+		foreach( $data as $key => $value ) {
+			$data[ $key ] = parse_data( $value ) ;
 		}
 		$_output = json_encode( $data ) ;
 	} else {
 		// this should generate some form of error object
 		$_output = 'unknown data type' ;
 	}
-	
+
+	if( isset( $options[ 'callback' ]) && !empty( $options[ 'callback' ])) {
+		$_output = "{$options[ 'callback' ]}({$_output})" ;
+	}
+
 	print( $_output ) ;
+	exit() ;
+
+function parse_data( $data ) {
+	$_output = $data ;
+	if( is_object( $data )) {
+		if( in_array( 'ORM', class_parents( $data ))) {
+			$_output = $data->as_array() ;
+		} elseif( in_array( 'Database_Result', class_parents( $data ))) {
+			$_output = $data->as_complete_array() ;
+		}
+	} elseif ( is_array( $data )) {
+		$_output = array() ;
+		foreach( $data as $key => $value ) {
+			$_output[ $key ] = parse_data( $value ) ;
+		}
+	}
+	return $_output ;
+}
+
 ?>
