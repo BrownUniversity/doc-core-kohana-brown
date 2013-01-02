c0d730ffe14b0ff55bc0bb21e083a3e688da219f
Jason Orrill <jason_orrill@brown.edu>
2013-01-02 16:55:26 +0000

    - Our standard DB config puts the port in the hostname ("host:port"), but apparently mysqli doesn't know how to deal with that. We now check for a port in the hostname and separate if necessary.
    git-svn-id: https://svn.brown.edu/svn/doc/k3_doc_module/trunk@4714 08eec777-141c-45f6-c71f-d277ddbd00ce

--- classes/doc/util/mysql.php
+++ classes/doc/util/mysql.php
@@ -6,63 +6,76 @@ defined('SYSPATH') or die('No direct script access.');
 
 /**
  * DOC_Util_Mysql
- * 
- * Used to allow ad-hoc querying of MySQL database that the Kohana 3 
+ *
+ * Used to allow ad-hoc querying of MySQL database that the Kohana 3
  * framework doesn't seem to give us.  By default, Auto Commit will be off.
- * 
+ *
  * @author Christopher Keith <Christopher_Keith@brown.edu>
  */
 class DOC_Util_Mysql {
-    
+
     private static $instances = array();
-    
+
     private $mysqli;
-    
+
     private $statements = array();
-    
+
     /**
      * Class constructor
-     * 
+     *
      * @param string $config database configuration name
      * @throws Kohana_Exception
      * @todo figure out non-standard port usage
      */
     private function __construct($config = 'default') {
-        
+
         $config = Kohana::$config->load("database.{$config}.connection");
-        
+
         $this->mysqli = mysqli_init();
-        
+
         /**
          * Allow the use of LOAD DATA LOCAL INFILE command
          */
         $this->mysqli->options(MYSQLI_OPT_LOCAL_INFILE, TRUE);
-        
+
+        /**
+         * Apparently mysqli doesn't like getting ports as part of the
+         * hostname, so we need to check the hostname for a port, and if
+         * it exists then separate the two into separate variables.
+         */
+        $port = NULL ;
+		$hostname = $config['hostname'] ;
+        if( preg_match('/^(.*?):(\d+)$/', $hostname, $matches)) {
+        	$hostname = $matches[1] ;
+			$port = $matches[2] ;
+        }
+
         $this->mysqli->real_connect(
-                $config['hostname'],
+                $hostname,
                 $config['username'],
                 $config['password'],
-                $config['database']
+                $config['database'],
+                $port
         );
-        
+
         if ($this->mysqli->connect_errno) {
             $msg = "Failed to connect to MySQL: " . $this->mysqli->connect_error;
             throw new Kohana_Exception($msg);
         }
-        
+
         $this->mysqli->autocommit(FALSE);
     }
-    
+
     /**
      * Class destructor
      */
     public function __destruct() {
         $this->mysqli->close();
     }
-    
+
     /**
      * Singleton pattern?
-     * 
+     *
      * @param string $config
      * @return DOC_Util_Mysql
      */
@@ -73,38 +86,38 @@ class DOC_Util_Mysql {
 
         return self::$instances[$config];
     }
-    
+
     /**
      * Commit a transaction
-     * 
+     *
      * @return boolean
      */
     public function commit() {
         return $this->mysqli->query('COMMIT;');
     }
-    
+
     /**
      * Use file transfer to achieve bulk data inserts/updates
-     * 
+     *
      * @param string $path path to local file
      * @param string $table name of table to update
      * @param array $columns ordered list of columns to update
      * @return ?
      */
     public function load($path, $table, $columns) {
-        
+
         $columns = implode(', ', $columns);
         $sql = "
             LOAD DATA LOCAL INFILE '{$path}'
             REPLACE INTO TABLE {$table}
             FIELDS TERMINATED BY '\t' ENCLOSED BY ''
             LINES TERMINATED BY '\n'
-            ({$columns}) 
+            ({$columns})
         ";
-        
+
             return $this->mysqli->query($sql);
     }
-    
+
     /**
      * Execute an arbitrary sequel query
      * @param type $sql
@@ -113,58 +126,58 @@ class DOC_Util_Mysql {
     public function query($sql) {
         return $this->mysqli->query($sql);
     }
-    
+
     /**
      * Prepare a statement for multiple executions
-     * 
+     *
      * @throws Kohana_Exception
      * @param string $sql
      * @return MySQLi Statment
      */
     public function prepare($sql) {
         $key = hash('sha256', $sql);
-        
+
         if ( ! array_key_exists($key, $this->statements)) {
             $this->statements[$key] = $this->mysqli->prepare($sql);
         }
-        
+
         if ($this->statements[$key] === FALSE) {
             throw new Kohana_Exception('MySQL: preparing statement failed.');
         }
-        
+
         return $this->statements[$key];
     }
-    
+
     /**
      * Remove a set of records from a table (used in provisioning)
-     * 
+     *
      * @param string $table
      * @param string $key
      * @param string $values
      */
     public function purge($table, $key, $values) {
         $values = implode(', ', $values);
-        
+
         $sql = "
             DELETE FROM {$table}
             WHERE {$key} IN ({$values})
         ";
-        
+
         return $this->mysqli->query($sql);
     }
-    
+
     /**
      * Rollback a transaction
-     * 
+     *
      * @return boolean
      */
     public function rollback() {
         return $this->mysqli->query('ROLLBACK;');
     }
-    
+
     /**
      * Begin a transaction
-     * 
+     *
      * @return boolean
      */
     public function start() {
