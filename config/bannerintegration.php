60cf66bfed3abe192075681f2a1d39063668b9a4
Christopher Keith <christopher_keith@brown.edu>
2013-01-09 21:44:26 +0000

    - modified Banner Integration configuration to separate out data for import and export routines
    git-svn-id: https://svn.brown.edu/svn/doc/k3_doc_module/trunk@4742 08eec777-141c-45f6-c71f-d277ddbd00ce

--- config/bannerintegration.php
+++ config/bannerintegration.php
@@ -18,26 +18,28 @@ return array(
     'username' => '***REMOVED***',
     'password' => '***REMOVED***',
     'imports'  => array(
-	'cohorts'		=> array(
-            'filename' => 'ask_cohort.dat', 
-            'regex' => '#\{\s*"cohort_code"\s*:\s*"[^"]*"\s*,\s*"cohort_description"\s*:\s*"[^"]*"\s*\}#'
-	),
-	'courses'		=> array(
-            'filename' => 'ask_courses.dat', 
-            'regex' => '#\{\s*"subject_code"\s*:\s*"[^"]*"\s*,\s*"subject_description"\s*:\s*"[^"]*"\s*,\s*"course_number"\s*:\s*"[^"]*"\s*,\s*"start_term"\s*:\s*"[^"]*"\s*,\s*"end_term"\s*:\s*"[^"]*"\s*,\s*"status"\s*:\s*"[^"]*"\s*,\s*"short_title"\s*:\s*"[^"]*"\s*,\s*"long_title"\s*:\s*"[^"]*"\s*\}#'
-	),
-	'instructors'	=> array(
-            'filename' => 'ask_instructors.dat', 
-            'regex' => '#\{\s*"Term"\s*:\s*"[^"]*"\s*,\s*"CRN"\s*:\s*"[^"]*"\s*,\s*"LDAP_ID"\s*:\s*"[^"]*"\s*,\s*"Primary_Ind"\s*:\s*"[^"]*"\s*\}#'
-	),
-	'students'		=> array(
-            'filename' => 'ask_student.dat', 
-            'regex' => '#\{\s*"Brown_ID"(.*?)"Cohorts"\s*:\s*\[(.*?)\]\s*,\s*"Attributes"\s*:\s*\[(.*?)\]\s*,\s*"Advisors"\s*:\s*\[(.*?)\]\s*,\s*"Programs"\s*:\s*\[(.*?)\]\s*,\s*"Transfer_Work"\s*:\s*\[(.*?)\]\s*,\s*"Courses"\s*:\s*\[(.*?)\]\s*\}#'
-	),
-	'writing'		=> array(
+        'cohorts'		=> array(
+                'filename' => 'ask_cohort.dat', 
+                'regex' => '#\{\s*"cohort_code"\s*:\s*"[^"]*"\s*,\s*"cohort_description"\s*:\s*"[^"]*"\s*\}#'
+        ),
+        'courses'		=> array(
+                'filename' => 'ask_courses.dat', 
+                'regex' => '#\{\s*"subject_code"\s*:\s*"[^"]*"\s*,\s*"subject_description"\s*:\s*"[^"]*"\s*,\s*"course_number"\s*:\s*"[^"]*"\s*,\s*"start_term"\s*:\s*"[^"]*"\s*,\s*"end_term"\s*:\s*"[^"]*"\s*,\s*"status"\s*:\s*"[^"]*"\s*,\s*"short_title"\s*:\s*"[^"]*"\s*,\s*"long_title"\s*:\s*"[^"]*"\s*\}#'
+        ),
+        'instructors'	=> array(
+                'filename' => 'ask_instructors.dat', 
+                'regex' => '#\{\s*"Term"\s*:\s*"[^"]*"\s*,\s*"CRN"\s*:\s*"[^"]*"\s*,\s*"LDAP_ID"\s*:\s*"[^"]*"\s*,\s*"Primary_Ind"\s*:\s*"[^"]*"\s*\}#'
+        ),
+        'students'		=> array(
+                'filename' => 'ask_student.dat', 
+                'regex' => '#\{\s*"Brown_ID"(.*?)"Cohorts"\s*:\s*\[(.*?)\]\s*,\s*"Attributes"\s*:\s*\[(.*?)\]\s*,\s*"Advisors"\s*:\s*\[(.*?)\]\s*,\s*"Programs"\s*:\s*\[(.*?)\]\s*,\s*"Transfer_Work"\s*:\s*\[(.*?)\]\s*,\s*"Courses"\s*:\s*\[(.*?)\]\s*\}#'
+        ),
+    ),
+    'exports' => array(
+        'writing'		=> array(
             'filename' => 'banner_writing.dat', 
             'regex' => ''
-	),
+        ),
     ),
 );
 
