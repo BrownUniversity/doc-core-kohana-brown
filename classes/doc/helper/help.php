478b7d942f14be791e1cb2e7083c7d870d68a3a5
Jason Orrill <jason_orrill@brown.edu>
2013-02-11 19:30:52 +0000

    - Updated DOC_Helper_Help to support help content at a level finer than just per-page. To best leverage this, use the DOC_Helper_Help::trigger() method to insert trigger spans instead of hard-coding them in the app. This will allow the class to populate a static array that contains the various context flags passed in. The help file structure now supports the notion of context files under the action directory. For example: help/foo/bar.php would be the place for the page-level help content, while help/foo/bar/section01.php would be the place for content identified as "section01" in the trigger.
    git-svn-id: https://svn.brown.edu/svn/doc/k3_doc_module/trunk@5054 08eec777-141c-45f6-c71f-d277ddbd00ce

--- classes/doc/helper/help.php
+++ classes/doc/helper/help.php
@@ -11,6 +11,30 @@
  */
 class DOC_Helper_Help {
 	
+	const HELP_ID = 'help' ;
+	const TRIGGER_ID = 'helpTrigger' ;
+	const TRIGGER_CLASS = 'help-trigger' ;
+	const NO_FLOAT_CLASS = 'nofloat' ;
+	
+	static $contexts = array() ;
+	
+	public static function trigger($context = NULL, $no_float = FALSE) {
+		self::$contexts[] = $context ;
+		
+		$trigger_id = self::TRIGGER_ID ;
+		$trigger_classes = array(self::TRIGGER_CLASS) ;
+		
+		if( !empty( $context )) {
+			$trigger_id .= "-{$context}" ;
+		}
+		
+		if( $no_float ) {
+			$trigger_classes[] = self::NO_FLOAT_CLASS ;
+		}
+		print("<span id='{$trigger_id}' class='".implode(' ', $trigger_classes)."'>?</span>") ;
+	}
+	
+	
 	/**
 	 * Look for the help content and output into the help div, or display "No help
 	 * available" if no file can be found.
@@ -19,17 +43,37 @@ class DOC_Helper_Help {
 		$request = Request::current() ;
 		$help_content = "<em>No help available</em>" ;
 		
-		$help_file = 'help/' ;
+		$help_file_root = 'help/' ;
 		$directory = $request->directory() ;
 		if( !empty( $directory )) {
-			$help_file .= $directory . '/' ;
+			$help_file_root .= $directory . '/' ;
 		}
 		
-		$help_file .= $request->controller().'/'.$request->action() ;
-		if( Kohana::find_file('views', $help_file) ) {	
-			$help_content = View::factory($help_file)->render() ;
+		if( !is_array( self::$contexts )) {
+			self::$contexts = array(self::$contexts) ;
 		}
 		
-		print("<div id='help'>{$help_content}</div>") ;
+		// make sure we include the page-level context, even if it's not explicitly included.
+		if( !in_array(NULL,array_values(self::$contexts))) {
+			self::$contexts[] = NULL ;
+		}
+		
+		
+		foreach( self::$contexts as $context ) {
+			if( empty( $context )) {
+				$help_id = self::HELP_ID ;
+				$help_file = $help_file_root . $request->controller().'/'.$request->action() ;
+			} else {
+				$help_id = self::HELP_ID . '-' . $context ;
+				$help_file = $help_file_root . $request->controller().'/'.$request->action().'/'.$context ;
+			}
+		
+			if( Kohana::find_file('views', $help_file) ) {	
+				$help_content = View::factory($help_file)->render() ;
+			}
+		
+			print("<div id='{$help_id}' class='help'>{$help_content}</div>") ;
+		
+		}
 	}
 }
