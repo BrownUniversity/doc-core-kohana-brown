bc884bec0dce5d50da3744ef51da10ebd44404dc
Jason Orrill <jason_orrill@brown.edu>
2013-05-21 20:38:20 +0000

    - Implemented DOC_Helper_Help::intro() to give us a hook to pull in intro content.
    git-svn-id: https://svn.brown.edu/svn/doc/k3_doc_module/trunk@5943 08eec777-141c-45f6-c71f-d277ddbd00ce

--- classes/doc/helper/help.php
+++ classes/doc/helper/help.php
@@ -12,8 +12,9 @@
 class DOC_Helper_Help {
 	
 	const HELP_ID = 'help' ;
-	const TRIGGER_ID = 'helpTrigger' ;
-	const TRIGGER_CLASS = 'help-trigger' ;
+	const INTRO_ID = 'intro' ;
+	const HELP_TRIGGER_ID = 'helpTrigger' ;
+	const HELP_TRIGGER_CLASS = 'help-trigger' ;
 	const NO_FLOAT_CLASS = 'nofloat' ;
 	
 	static $contexts = array() ;
@@ -21,8 +22,8 @@ class DOC_Helper_Help {
 	public static function trigger($context = NULL, $no_float = FALSE) {
 		self::$contexts[] = $context ;
 		
-		$trigger_id = self::TRIGGER_ID ;
-		$trigger_classes = array(self::TRIGGER_CLASS) ;
+		$trigger_id = self::HELP_TRIGGER_ID ;
+		$trigger_classes = array(self::HELP_TRIGGER_CLASS) ;
 		
 		if( !empty( $context )) {
 			$trigger_id .= "-{$context}" ;
@@ -74,6 +75,37 @@ class DOC_Helper_Help {
 		
 			print("<div id='{$help_id}' class='help'>{$help_content}</div>") ;
 		
+		}
+	}
+	
+	/**
+	 * Look for intro content and if it exists put into a div. Note that applications using this
+	 * should also track whether the user has opted to turn off the intro content. Child classes
+	 * will be responsible for that logic.
+	 */
+	public static function intro($app_id = '', $path_fragment = NULL) {
+		
+		$request = Request::current() ;
+		$intro_file_root = 'intro/' ;
+		
+		if( empty( $path_fragment )) {
+			$path_fragment = '' ;
+		
+			$directory = $request->directory() ;
+			if( !empty( $directory )) {
+				$path_fragment .= $directory . '/' ;
+			}
+			$path_fragment .= $request->controller().'/'.$request->action() ;		
+		}
+		
+		$intro_file = $intro_file_root . $path_fragment ;
+		
+		if( Kohana::find_file('views', $intro_file) ) {	
+			$intro_content = View::factory($intro_file)->render() ;
+			print('<div id="'.self::INTRO_ID.'" class="intro" data-appid="'.$app_id.'" data-path="'.$path_fragment.'">') ;
+			print($intro_content) ;
+			print("<a class='do-not-show'>Do not show again</a>") ;
+			print("</div>") ;
 		}
 	}
 }
