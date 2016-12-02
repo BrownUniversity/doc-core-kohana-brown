ad6eb04cf255113cc6514b8dc7bcd4352784dda7
Orrill, Jason <jason_orrill@brown.edu>
2016-12-02 16:24:51 -0500

    - Added support for logging to Slack. Note that some data is commented out in DOC_Log_Slack, since it depends application-specific code and data. I left it in for reference in case people want to override the method. [DCORE-7]

---
+++ classes/doc/util/slack.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package DOC core
+ * @version 1.0
+ * @since 1.0
+ */
+
+class DOC_Util_Slack {
+	/**
+     * Send a message to a slack channel
+     *
+     * @param string $url
+     * @param string $message
+     */
+    public static function send($url, $message)
+    {
+        $msg = array(
+            'text' => $message,
+        );
+
+        $opts = array('http' =>
+            array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => json_encode($msg)
+            )
+        );
+
+        $context  = stream_context_create($opts);
+
+        // Intentionally surpressing errors as errors are not important in this context
+        @file_get_contents($url, false, $context);
+    }
+}
