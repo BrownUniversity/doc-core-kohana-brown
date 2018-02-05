4cd2a843e85a7330fdda7346586bb86913eaa100
Chris Keith <Christopher_Keith@brown.edu>
2018-02-05 12:16:49 -0500

    DCORE-26 implementing use of transport generator in the normal send message and spool processing methods.

--- classes/doc/util/mail.php
+++ classes/doc/util/mail.php
@@ -28,9 +28,7 @@ class DOC_Util_Mail {
 			$spool = new Swift_FileSpool($mail_config['spool_location']) ;
 			$mailer = new Swift_Mailer( new Swift_SpoolTransport( $spool )) ;
 		} else {
-// 			$transport = Swift_MailTransport::newInstance() ;
-// 			$transport = Swift_SmtpTransport::newInstance('localhost') ;
-			$transport = Swift_SendmailTransport::newInstance() ;
+		    $transport = self::getDeliveryTransport($mail_config);
 			$mailer = Swift_Mailer::newInstance($transport) ;
 		}
 
@@ -127,9 +125,7 @@ class DOC_Util_Mail {
 		$mail_config = Kohana::$config->load('mail') ;
 
 		$spool = new Swift_FileSpool($mail_config['spool_location']) ;
-// 		$transport = Swift_MailTransport::newInstance() ;
-// 		$transport = Swift_SmtpTransport::newInstance('localhost') ;
-		$transport = Swift_SendmailTransport::newInstance() ;
+		$transport = self::getDeliveryTransport($mail_config);
 		$count = $spool->flushQueue($transport) ;
 		Kohana::$log->add(Log::INFO, "Flushed {$count} messages from the spool.") ;
 	}
